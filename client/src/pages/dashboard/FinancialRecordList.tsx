52bdbd64635efab3c1eb258a7397658ca6bddd2b
Ravi <ravi@Ravis-MacBook-Air.local>
2025-03-14 08:00:35 +0545

    financial record form created

---
+++ client/src/pages/dashboard/FinancialRecordList.tsx
@@ -0,0 +1,7 @@
+export const FinacialRecordList = () => {
+    return (
+        <div>
+            <h1>Finacial Record List</h1>
+        </div>
+    )
+}
--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import {useUser} from "@clerk/clerk-react"
+import { FinacialRecordForm } from "./FinancialRecordForm";
+import { FinacialRecordList } from "./FinancialRecordList";
 export const Dashboard = () => {
     const {user} = useUser();
     return (
         <div className="dashboard-container">
             <h1>Welcome {user?.firstName}! Here are your finances:</h1>
+            <FinacialRecordForm/>
+            <FinacialRecordList/>
         </div>
     )
 }
